e26449424be9709f6d21eae1630e89765b0da79f
Alejandro Iglesias Calvo <uo247346@uniovi.es>
2020-04-18 15:40:40 +0200

    A ver si este finde empiezo con la practica ya

---
+++ sdi1920-entrega2-506.js
@@ -0,0 +1,15 @@
+//Modulos
+let express = require('express');
+let app = express();
+
+let rest = require('request');
+app.set('rest',rest);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // Debemos especificar todas las headers que se aceptan. Content-Type , token
+    next();
+});
