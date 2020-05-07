a8ff1ab0f9617e9356f262e829f56462d76cacec
Alejandro Iglesias Calvo <uo247346@uniovi.es>
2020-05-07 17:55:45 +0200

    Mañana mas

--- sdi1920-entrega2-test-506/src/com/uniovi/tests/sdi1920entrega2test506.java
+++ sdi1920-entrega2-test-506/src/com/uniovi/tests/sdi1920entrega2test506.java
@@ -67,178 +67,178 @@ public class sdi1920entrega2test506 {
 		SeleniumUtils.textoPresentePagina(driver, "Nuevo usuario registrado");
 	}
 
-	// PR02. Sin hacer /
-	@Test
-	public void PR02() {
-		assertTrue("PR02 sin hacer", false);
-	}
-
-	// PR03. Sin hacer /
-	@Test
-	public void PR03() {
-		assertTrue("PR03 sin hacer", false);
-	}
-
-	// PR04. Sin hacer /
-	@Test
-	public void PR04() {
-		assertTrue("PR04 sin hacer", false);
-	}
-
-	// PR05. Sin hacer /
-	@Test
-	public void PR05() {
-		assertTrue("PR05 sin hacer", false);
-	}
-
-	// PR06. Sin hacer /
-	@Test
-	public void PR06() {
-		assertTrue("PR06 sin hacer", false);
-	}
-
-	// PR07. Sin hacer /
-	@Test
-	public void PR07() {
-		assertTrue("PR07 sin hacer", false);
-	}
-
-	// PR08. Sin hacer /
-	@Test
-	public void PR08() {
-		assertTrue("PR08 sin hacer", false);
-	}
-
-	// PR09. Sin hacer /
-	@Test
-	public void PR09() {
-		assertTrue("PR09 sin hacer", false);
-	}
-
-	// PR10. Sin hacer /
-	@Test
-	public void PR10() {
-		assertTrue("PR10 sin hacer", false);
-	}
-
-	// PR11. Sin hacer /
-	@Test
-	public void PR11() {
-		assertTrue("PR11 sin hacer", false);
-	}
-
-	// PR12. Sin hacer /
-	@Test
-	public void PR12() {
-		assertTrue("PR12 sin hacer", false);
-	}
-
-	// PR13. Sin hacer /
-	@Test
-	public void PR13() {
-		assertTrue("PR13 sin hacer", false);
-	}
-
-	// PR14. Sin hacer /
-	@Test
-	public void PR14() {
-		assertTrue("PR14 sin hacer", false);
-	}
-
-	// PR15. Sin hacer /
-	@Test
-	public void PR15() {
-		assertTrue("PR15 sin hacer", false);
-	}
-
-	// PR16. Sin hacer /
-	@Test
-	public void PR16() {
-		assertTrue("PR16 sin hacer", false);
-	}
-
-	// PR017. Sin hacer /
-	@Test
-	public void PR17() {
-		assertTrue("PR17 sin hacer", false);
-	}
-
-	// PR18. Sin hacer /
-	@Test
-	public void PR18() {
-		assertTrue("PR18 sin hacer", false);
-	}
-
-	// PR19. Sin hacer /
-	@Test
-	public void PR19() {
-		assertTrue("PR19 sin hacer", false);
-	}
-
-	// P20. Sin hacer /
-	@Test
-	public void PR20() {
-		assertTrue("PR20 sin hacer", false);
-	}
-
-	// PR21. Sin hacer /
-	@Test
-	public void PR21() {
-		assertTrue("PR21 sin hacer", false);
-	}
-
-	// PR22. Sin hacer /
-	@Test
-	public void PR22() {
-		assertTrue("PR22 sin hacer", false);
-	}
-
-	// PR23. Sin hacer /
-	@Test
-	public void PR23() {
-		assertTrue("PR23 sin hacer", false);
-	}
-
-	// PR24. Sin hacer /
-	@Test
-	public void PR24() {
-		assertTrue("PR24 sin hacer", false);
-	}
-
-	// PR25. Sin hacer /
-	@Test
-	public void PR25() {
-		assertTrue("PR25 sin hacer", false);
-	}
-
-	// PR26. Sin hacer /
-	@Test
-	public void PR26() {
-		assertTrue("PR26 sin hacer", false);
-	}
-
-	// PR27. Sin hacer /
-	@Test
-	public void PR27() {
-		assertTrue("PR27 sin hacer", false);
-	}
-
-	// PR029. Sin hacer /
-	@Test
-	public void PR29() {
-		assertTrue("PR29 sin hacer", false);
-	}
-
-	// PR030. Sin hacer /
-	@Test
-	public void PR30() {
-		assertTrue("PR30 sin hacer", false);
-	}
-
-	// PR031. Sin hacer /
-	@Test
-	public void PR31() {
-		assertTrue("PR31 sin hacer", false);
-	}
+//	// PR02. Sin hacer /
+//	@Test
+//	public void PR02() {
+//		assertTrue("PR02 sin hacer", false);
+//	}
+//
+//	// PR03. Sin hacer /
+//	@Test
+//	public void PR03() {
+//		assertTrue("PR03 sin hacer", false);
+//	}
+//
+//	// PR04. Sin hacer /
+//	@Test
+//	public void PR04() {
+//		assertTrue("PR04 sin hacer", false);
+//	}
+//
+//	// PR05. Sin hacer /
+//	@Test
+//	public void PR05() {
+//		assertTrue("PR05 sin hacer", false);
+//	}
+//
+//	// PR06. Sin hacer /
+//	@Test
+//	public void PR06() {
+//		assertTrue("PR06 sin hacer", false);
+//	}
+//
+//	// PR07. Sin hacer /
+//	@Test
+//	public void PR07() {
+//		assertTrue("PR07 sin hacer", false);
+//	}
+//
+//	// PR08. Sin hacer /
+//	@Test
+//	public void PR08() {
+//		assertTrue("PR08 sin hacer", false);
+//	}
+//
+//	// PR09. Sin hacer /
+//	@Test
+//	public void PR09() {
+//		assertTrue("PR09 sin hacer", false);
+//	}
+//
+//	// PR10. Sin hacer /
+//	@Test
+//	public void PR10() {
+//		assertTrue("PR10 sin hacer", false);
+//	}
+//
+//	// PR11. Sin hacer /
+//	@Test
+//	public void PR11() {
+//		assertTrue("PR11 sin hacer", false);
+//	}
+//
+//	// PR12. Sin hacer /
+//	@Test
+//	public void PR12() {
+//		assertTrue("PR12 sin hacer", false);
+//	}
+//
+//	// PR13. Sin hacer /
+//	@Test
+//	public void PR13() {
+//		assertTrue("PR13 sin hacer", false);
+//	}
+//
+//	// PR14. Sin hacer /
+//	@Test
+//	public void PR14() {
+//		assertTrue("PR14 sin hacer", false);
+//	}
+//
+//	// PR15. Sin hacer /
+//	@Test
+//	public void PR15() {
+//		assertTrue("PR15 sin hacer", false);
+//	}
+//
+//	// PR16. Sin hacer /
+//	@Test
+//	public void PR16() {
+//		assertTrue("PR16 sin hacer", false);
+//	}
+//
+//	// PR017. Sin hacer /
+//	@Test
+//	public void PR17() {
+//		assertTrue("PR17 sin hacer", false);
+//	}
+//
+//	// PR18. Sin hacer /
+//	@Test
+//	public void PR18() {
+//		assertTrue("PR18 sin hacer", false);
+//	}
+//
+//	// PR19. Sin hacer /
+//	@Test
+//	public void PR19() {
+//		assertTrue("PR19 sin hacer", false);
+//	}
+//
+//	// P20. Sin hacer /
+//	@Test
+//	public void PR20() {
+//		assertTrue("PR20 sin hacer", false);
+//	}
+//
+//	// PR21. Sin hacer /
+//	@Test
+//	public void PR21() {
+//		assertTrue("PR21 sin hacer", false);
+//	}
+//
+//	// PR22. Sin hacer /
+//	@Test
+//	public void PR22() {
+//		assertTrue("PR22 sin hacer", false);
+//	}
+//
+//	// PR23. Sin hacer /
+//	@Test
+//	public void PR23() {
+//		assertTrue("PR23 sin hacer", false);
+//	}
+//
+//	// PR24. Sin hacer /
+//	@Test
+//	public void PR24() {
+//		assertTrue("PR24 sin hacer", false);
+//	}
+//
+//	// PR25. Sin hacer /
+//	@Test
+//	public void PR25() {
+//		assertTrue("PR25 sin hacer", false);
+//	}
+//
+//	// PR26. Sin hacer /
+//	@Test
+//	public void PR26() {
+//		assertTrue("PR26 sin hacer", false);
+//	}
+//
+//	// PR27. Sin hacer /
+//	@Test
+//	public void PR27() {
+//		assertTrue("PR27 sin hacer", false);
+//	}
+//
+//	// PR029. Sin hacer /
+//	@Test
+//	public void PR29() {
+//		assertTrue("PR29 sin hacer", false);
+//	}
+//
+//	// PR030. Sin hacer /
+//	@Test
+//	public void PR30() {
+//		assertTrue("PR30 sin hacer", false);
+//	}
+//
+//	// PR031. Sin hacer /
+//	@Test
+//	public void PR31() {
+//		assertTrue("PR31 sin hacer", false);
+//	}
 
 }
