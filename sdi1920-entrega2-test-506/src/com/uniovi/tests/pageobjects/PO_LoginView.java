371c6e01122b2a0c8a54af9980a0e893440ff549
Alejandro Iglesias Calvo <uo247346@uniovi.es>
2020-05-07 17:07:45 +0200

    Creo que ya consegui añadir las page objetcs de una p*** vez

--- sdi1920-entrega2-test-506/src/com/uniovi/tests/pageobjects/PO_LoginView.java
+++ sdi1920-entrega2-test-506/src/com/uniovi/tests/pageobjects/PO_LoginView.java
@@ -4,20 +4,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.uniovi.tests.util.SeleniumUtils;
+
 public class PO_LoginView extends PO_NavView {
 
-	static public void fillForm(WebDriver driver, String dnip, String passwordp) {
-		WebElement dni = driver.findElement(By.name("username"));
-		dni.click();
-		dni.clear();
-		dni.sendKeys(dnip);
+	public static void fillForm(WebDriver driver, String emailp, String passwordp) {
+		// TODO Auto-generated method stub
+		WebElement email = driver.findElement(By.name("email"));
+		email.click();
+		email.clear();
+		email.sendKeys(emailp);
 		WebElement password = driver.findElement(By.name("password"));
 		password.click();
 		password.clear();
 		password.sendKeys(passwordp);
-		//Pulsar el boton de Alta.
+		// Pulsar el boton de Alta.
 		By boton = By.className("btn");
-		driver.findElement(boton).click();	
+		driver.findElement(boton).click();
+	}
+
+	static public void checkLogIn(WebDriver driver, int language) {
+		SeleniumUtils.EsperaCargaPagina(driver, "text", p.getString("login.message", language), getTimeout());
+	}
+
+	public static void checkInvalidLogIn(WebDriver driver, int language) {
+		SeleniumUtils.EsperaCargaPagina(driver, "text", p.getString("unsuccessfull.signin.message", language),
+				getTimeout());
 	}
 
 }
