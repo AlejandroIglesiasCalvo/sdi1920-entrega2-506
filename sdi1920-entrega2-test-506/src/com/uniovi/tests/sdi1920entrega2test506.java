04d75b5f834e37b9852761fc1de398ad12cb8458
Alejandro Iglesias Calvo <uo247346@uniovi.es>
2020-05-07 17:20:20 +0200

    No va

--- sdi1920-entrega2-test-506/src/com/uniovi/tests/sdi1920entrega2test506.java
+++ sdi1920-entrega2-test-506/src/com/uniovi/tests/sdi1920entrega2test506.java
@@ -1,4 +1,5 @@
 package com.uniovi.tests;
+
 //Paquetes Java
 import java.util.List;
 //Paquetes JUnit 
@@ -13,22 +14,14 @@ import com.uniovi.tests.util.SeleniumUtils;
 //Paquetes con los Page Object
 import com.uniovi.tests.pageobjects.*;
 
-
-//Ordenamos las pruebas por el nombre del método
-@FixMethodOrder(MethodSorters.NAME_ASCENDING) 
+//Ordenamos las pruebas por el nombre del mÃ©todo
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class sdi1920entrega2test506 {
-	//En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens automáticas)):
-	//static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-	//static String Geckdriver024 = "C:\\Path\\geckodriver024win64.exe";
-	//En MACOSX (Debe ser la versión 65.0.1 y desactivar las actualizacioens automáticas):
-	static String PathFirefox65 = "/Applications/Firefox 2.app/Contents/MacOS/firefox-bin";
-	//static String PathFirefox64 = "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
-	static String Geckdriver024 = "/Users/delacal/Documents/SDI1718/firefox/geckodriver024mac";
-	//static String Geckdriver022 = "/Users/delacal/Documents/SDI1718/firefox/geckodriver023mac";
-	//Común a Windows y a MACOSX
-	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024); 
-	static String URL = "https://localhost:8081";
+	static String PathFirefox65 = "D:\\ProgramasInstalados\\Firefox\\firefox.exe";
+	static String Geckdriver024 = "C:\\Users\\alejandro\\Downloads\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
 
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
+	static String URL = "https://localhost:8081";
 
 	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
 		System.setProperty("webdriver.firefox.bin", PathFirefox);
@@ -37,206 +30,215 @@ public class sdi1920entrega2test506 {
 		return driver;
 	}
 
-
 	@Before
-	public void setUp(){
+	public void setUp() {
 		driver.navigate().to(URL);
 	}
+
 	@After
-	public void tearDown(){
+	public void tearDown() {
 		driver.manage().deleteAllCookies();
 	}
-	@BeforeClass 
+
+	@BeforeClass
 	static public void begin() {
-		//COnfiguramos las pruebas.
-		//Fijamos el timeout en cada opción de carga de una vista. 2 segundos.
+		// COnfiguramos las pruebas.
+		// Fijamos el timeout en cada opciÃ³n de carga de una vista. 2 segundos.
 		PO_View.setTimeout(3);
 
 	}
+
 	@AfterClass
 	static public void end() {
-		//Cerramos el navegador al finalizar las pruebas
+		// Cerramos el navegador al finalizar las pruebas
 		driver.quit();
 	}
 
-	//PR01. Sin hacer /
+	// 1.1 [RegVal] Registro de Usuario con datos vÃ¡lidos
 	@Test
-	public void PR01() {
-		assertTrue("PR01 sin hacer", false);			
+	public void PR01_RegVal() {
+		// Vamos al formulario de registro
+		List<WebElement> elementos = PO_View.checkElement(driver, "free", "//li/a[contains(@id, 'botonSignup')]");
+		elementos.get(0).click();
+		SeleniumUtils.esperarSegundos(driver, 2);
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "RegVal", "prueba99@prueba99.com", "prueba99", "prueba99");
+		// Comprobamos que se ha registrado con Ã©xito
+		SeleniumUtils.textoPresentePagina(driver, "Nuevo usuario registrado");
 	}
 
-	//PR02. Sin hacer /
+	// PR02. Sin hacer /
 	@Test
 	public void PR02() {
-		assertTrue("PR02 sin hacer", false);			
+		assertTrue("PR02 sin hacer", false);
 	}
 
-	//PR03. Sin hacer /
+	// PR03. Sin hacer /
 	@Test
 	public void PR03() {
-		assertTrue("PR03 sin hacer", false);			
+		assertTrue("PR03 sin hacer", false);
 	}
-	
-	//PR04. Sin hacer /
+
+	// PR04. Sin hacer /
 	@Test
 	public void PR04() {
-		assertTrue("PR04 sin hacer", false);			
+		assertTrue("PR04 sin hacer", false);
 	}
-	
-	//PR05. Sin hacer /
+
+	// PR05. Sin hacer /
 	@Test
 	public void PR05() {
-		assertTrue("PR05 sin hacer", false);			
+		assertTrue("PR05 sin hacer", false);
 	}
-	
-	//PR06. Sin hacer /
+
+	// PR06. Sin hacer /
 	@Test
 	public void PR06() {
-		assertTrue("PR06 sin hacer", false);			
+		assertTrue("PR06 sin hacer", false);
 	}
-	
-	//PR07. Sin hacer /
+
+	// PR07. Sin hacer /
 	@Test
 	public void PR07() {
-		assertTrue("PR07 sin hacer", false);			
-	}	
-	
-	//PR08. Sin hacer /
+		assertTrue("PR07 sin hacer", false);
+	}
+
+	// PR08. Sin hacer /
 	@Test
 	public void PR08() {
-		assertTrue("PR08 sin hacer", false);			
-	}	
-	
-	//PR09. Sin hacer /
+		assertTrue("PR08 sin hacer", false);
+	}
+
+	// PR09. Sin hacer /
 	@Test
 	public void PR09() {
-		assertTrue("PR09 sin hacer", false);			
-	}	
-	//PR10. Sin hacer /
+		assertTrue("PR09 sin hacer", false);
+	}
+
+	// PR10. Sin hacer /
 	@Test
 	public void PR10() {
-		assertTrue("PR10 sin hacer", false);			
-	}	
-	
-	//PR11. Sin hacer /
+		assertTrue("PR10 sin hacer", false);
+	}
+
+	// PR11. Sin hacer /
 	@Test
 	public void PR11() {
-		assertTrue("PR11 sin hacer", false);			
-	}	
-	
-	//PR12. Sin hacer /
+		assertTrue("PR11 sin hacer", false);
+	}
+
+	// PR12. Sin hacer /
 	@Test
 	public void PR12() {
-		assertTrue("PR12 sin hacer", false);			
-	}	
-	
-	//PR13. Sin hacer /
+		assertTrue("PR12 sin hacer", false);
+	}
+
+	// PR13. Sin hacer /
 	@Test
 	public void PR13() {
-		assertTrue("PR13 sin hacer", false);			
-	}	
-	
-	//PR14. Sin hacer /
+		assertTrue("PR13 sin hacer", false);
+	}
+
+	// PR14. Sin hacer /
 	@Test
 	public void PR14() {
-		assertTrue("PR14 sin hacer", false);			
-	}	
-	
-	//PR15. Sin hacer /
+		assertTrue("PR14 sin hacer", false);
+	}
+
+	// PR15. Sin hacer /
 	@Test
 	public void PR15() {
-		assertTrue("PR15 sin hacer", false);			
-	}	
-	
-	//PR16. Sin hacer /
+		assertTrue("PR15 sin hacer", false);
+	}
+
+	// PR16. Sin hacer /
 	@Test
 	public void PR16() {
-		assertTrue("PR16 sin hacer", false);			
-	}	
-	
-	//PR017. Sin hacer /
+		assertTrue("PR16 sin hacer", false);
+	}
+
+	// PR017. Sin hacer /
 	@Test
 	public void PR17() {
-		assertTrue("PR17 sin hacer", false);			
-	}	
-	
-	//PR18. Sin hacer /
+		assertTrue("PR17 sin hacer", false);
+	}
+
+	// PR18. Sin hacer /
 	@Test
 	public void PR18() {
-		assertTrue("PR18 sin hacer", false);			
-	}	
-	
-	//PR19. Sin hacer /
+		assertTrue("PR18 sin hacer", false);
+	}
+
+	// PR19. Sin hacer /
 	@Test
 	public void PR19() {
-		assertTrue("PR19 sin hacer", false);			
-	}	
-	
-	//P20. Sin hacer /
+		assertTrue("PR19 sin hacer", false);
+	}
+
+	// P20. Sin hacer /
 	@Test
 	public void PR20() {
-		assertTrue("PR20 sin hacer", false);			
-	}	
-	
-	//PR21. Sin hacer /
+		assertTrue("PR20 sin hacer", false);
+	}
+
+	// PR21. Sin hacer /
 	@Test
 	public void PR21() {
-		assertTrue("PR21 sin hacer", false);			
-	}	
-	
-	//PR22. Sin hacer /
+		assertTrue("PR21 sin hacer", false);
+	}
+
+	// PR22. Sin hacer /
 	@Test
 	public void PR22() {
-		assertTrue("PR22 sin hacer", false);			
-	}	
-	
-	//PR23. Sin hacer /
+		assertTrue("PR22 sin hacer", false);
+	}
+
+	// PR23. Sin hacer /
 	@Test
 	public void PR23() {
-		assertTrue("PR23 sin hacer", false);			
-	}	
-	
-	//PR24. Sin hacer /
+		assertTrue("PR23 sin hacer", false);
+	}
+
+	// PR24. Sin hacer /
 	@Test
 	public void PR24() {
-		assertTrue("PR24 sin hacer", false);			
-	}	
-	//PR25. Sin hacer /
+		assertTrue("PR24 sin hacer", false);
+	}
+
+	// PR25. Sin hacer /
 	@Test
 	public void PR25() {
-		assertTrue("PR25 sin hacer", false);			
-	}	
-	
-	//PR26. Sin hacer /
+		assertTrue("PR25 sin hacer", false);
+	}
+
+	// PR26. Sin hacer /
 	@Test
 	public void PR26() {
-		assertTrue("PR26 sin hacer", false);			
-	}	
-	
-	//PR27. Sin hacer /
+		assertTrue("PR26 sin hacer", false);
+	}
+
+	// PR27. Sin hacer /
 	@Test
 	public void PR27() {
-		assertTrue("PR27 sin hacer", false);			
-	}	
-	
-	//PR029. Sin hacer /
+		assertTrue("PR27 sin hacer", false);
+	}
+
+	// PR029. Sin hacer /
 	@Test
 	public void PR29() {
-		assertTrue("PR29 sin hacer", false);			
+		assertTrue("PR29 sin hacer", false);
 	}
 
-	//PR030. Sin hacer /
+	// PR030. Sin hacer /
 	@Test
 	public void PR30() {
-		assertTrue("PR30 sin hacer", false);			
+		assertTrue("PR30 sin hacer", false);
 	}
-	
-	//PR031. Sin hacer /
+
+	// PR031. Sin hacer /
 	@Test
 	public void PR31() {
-		assertTrue("PR31 sin hacer", false);			
+		assertTrue("PR31 sin hacer", false);
 	}
-	
-		
-}
 
+}
